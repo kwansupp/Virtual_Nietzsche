f1e0bdf96c87e13cc637e64b21516192b0244f0b
ozzypossie <oskar.vanderwal@gmail.com>
2018-12-30 19:43:40 +0100

    Got a word predictor for 2 prior words

--- nietzsche_predict_word.py
+++ nietzsche_predict_word.py
@@ -1,30 +1,42 @@
 from collections import Counter
 import pandas as pd
+import os
 
 test_sentence = []
 
-with open('test_nietzsche.txt', 'r') as myfile:
-    for line in myfile:
-        for word in line.split():
-           test_sentence.append(word)
-
-print(test_sentence)
+for file in os.listdir('datasets'):
+    #print(file)
+    with open('./datasets/'+ file, 'r') as myfile:
+        for line in myfile:
+            for word in line.split():
+               test_sentence.append(word)
 
 trigrams = [(test_sentence[i], test_sentence[i + 1], test_sentence[i + 2])
             for i in range(len(test_sentence) - 2)]
 
-print(trigrams[:3])
+#print(trigrams[:3])
 
-print(len(trigrams))
+#print(len(trigrams))
 
-#TODO: count how often trigram occurs
-#myset = unique(tigrams)
-#print(myset)
+#Count how often trigram occurs
 
-df = pd.DataFrame({'col':trigrams})
-print(df)
-print(df.col.unique())
+trigrams_df = pd.DataFrame({'trigram':trigrams})
+#print(df)
+#print(df.col.unique())
+#print(df.col.value_counts())
 
-#counts = Counter(test_sentence)
-#print(counts)
-print(df.col.value_counts())
+val_col = trigrams_df.trigram.value_counts().keys().tolist()
+freq_col = trigrams_df.trigram.value_counts().tolist()
+
+trigram_freq = pd.DataFrame(columns=['trigrams','frequency'])
+trigram_freq['trigrams'] = val_col
+trigram_freq['frequency'] = freq_col
+#print(trigram_freq)
+
+#TODO function that returns all the trigrams starting with given two words
+def returnMostProbableWord(df, word1, word2):
+    print(df[df['trigrams'].apply(lambda x: True if "This" in x[0] and "is" in x[1] else False)]['trigrams'].iloc[0][2])
+
+returnMostProbableWord(trigram_freq, "Thus", "spake")
+
+#print(trigram_counts[trigram_counts["col"].apply(lambda x: True if "This" in x[0] and "is" in x[1] else False)])
