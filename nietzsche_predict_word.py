cd65d512667276baf4fc7a917718543425cca27c
ozzypossie <oskar.vanderwal@gmail.com>
2018-12-30 18:09:07 +0100

    finally got trigram counts working!

--- nietzsche_predict_word.py
+++ nietzsche_predict_word.py
@@ -1,12 +1,30 @@
+from collections import Counter
+import pandas as pd
+
+test_sentence = []
+
 with open('test_nietzsche.txt', 'r') as myfile:
-    #test_sentence=myfile.read().replace('\n', '')
     for line in myfile:
         for word in line.split():
-           print(word)
+           test_sentence.append(word)
 
-#print(test_sentence)
+print(test_sentence)
 
-trigrams = [([test_sentence[i], test_sentence[i + 1]], test_sentence[i + 2])
+trigrams = [(test_sentence[i], test_sentence[i + 1], test_sentence[i + 2])
             for i in range(len(test_sentence) - 2)]
-# print the first 3, just so you can see what they look like
-#print(trigrams[:3])
+
+print(trigrams[:3])
+
+print(len(trigrams))
+
+#TODO: count how often trigram occurs
+#myset = unique(tigrams)
+#print(myset)
+
+df = pd.DataFrame({'col':trigrams})
+print(df)
+print(df.col.unique())
+
+#counts = Counter(test_sentence)
+#print(counts)
+print(df.col.value_counts())
